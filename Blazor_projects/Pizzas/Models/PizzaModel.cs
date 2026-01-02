74b3bc7c916878a3019a05a3c25999ba65923810
hassanNewton <hassan.hussin@newton.se>
2026-01-02 13:24:21 +0100

    Pizza project done

---
+++ Blazor_projects/Pizzas/Models/PizzaModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Pizzas.Models;
+
+public class PizzaModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Deliciousness { get; set; }
+    public string ImageUrl { get; set; }
+}
+
+//Detta är exakt den struktur JSON-datan kommer ha
+
+//Blazor kan automatiskt mappa JSON → objekt
