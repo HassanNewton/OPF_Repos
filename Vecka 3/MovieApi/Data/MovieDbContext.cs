972d71530cddfe2d3202df12a99b2de78eb6626c
hassanNewton <hassan.hussin@newton.se>
2026-01-27 10:37:47 +0100

    .

--- Vecka 3/MovieApi/Data/MovieDbContext.cs
+++ Vecka 3/MovieApi/Data/MovieDbContext.cs
@@ -18,3 +18,56 @@ namespace MovieApi.Data
 }
 //DbSet < Movie > = tabellen Movies
 //DbSet < Director > = tabellen Directors
+
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    // Directors
+    modelBuilder.Entity<Director>().HasData(
+        new Director { Id = 1, Name = "Jim Henson, Frank Oz", Nationality = "American" },
+        new Director { Id = 2, Name = "The Wachowski Brothers", Nationality = "American" },
+        new Director { Id = 3, Name = "Guillermo del Toro", Nationality = "Mexican" },
+        new Director { Id = 4, Name = "Wolfgang Petersen", Nationality = "German" },
+        new Director { Id = 5, Name = "Ridley Scott", Nationality = "British" }
+    );
+
+    // Movies
+    modelBuilder.Entity<Movie>().HasData(
+        new Movie
+        {
+            Id = 1,
+            Title = "The Dark Crystal",
+            Description = "The last of the Gelflings...",
+            DirectorId = 1
+        },
+        new Movie
+        {
+            Id = 2,
+            Title = "The Matrix",
+            Description = "A hacker named Neo discovers...",
+            DirectorId = 2
+        },
+        new Movie
+        {
+            Id = 3,
+            Title = "Pan's Labyrinth",
+            Description = "In the falangist Spain of 1944...",
+            DirectorId = 3
+        },
+        new Movie
+        {
+            Id = 4,
+            Title = "The NeverEnding Story",
+            Description = "A young boy becomes trapped in a book...",
+            DirectorId = 4
+        },
+        new Movie
+        {
+            Id = 5,
+            Title = "Blade Runner",
+            Description = "In a dystopian Los Angeles...",
+            DirectorId = 5
+        }
+    );
+}
+}
+ 
--- Vecka 4/MovieMVCDemo/Controllers/HomeController.cs
+++ Vecka 4/MovieMVCDemo/Controllers/HomeController.cs
@@ -23,3 +23,22 @@ namespace MovieMVCDemo.Controllers
         }
     }
 }
+
+
+//Skapa en controller ex ColorsController
+//Skapa action metoder som returnerar VIEWS
+//Skapa en view mapp med samma namn som controllern under VIEW mappen
+//VIKTIGT: se till att ha views med samma namn som action under views mappen
+//I dem olika views filerna kan man länka till nästa view mha asp-controller och asp-action
+//Lägg in i layout filen för att kunna navigera till Colors
+
+//asp-action = vilken metod? 
+//asp-controller = vilken controller/klass
+//asp-route-id = vilket värde som skickas (parameter)
+//asp-for koppa/binda input till modellen
+
+//BOOTSTRAP FÄRG KLASSER
+//bg-primary = blå
+//bg-sucess = grön
+//bg-warning = Gul
+//bg-danger = Röd
