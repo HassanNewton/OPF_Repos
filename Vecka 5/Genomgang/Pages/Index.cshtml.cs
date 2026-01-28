77db10dc51744d4d88aa484625acc58f287a152e
hassanNewton <hassan.hussin@newton.se>
2026-01-28 17:36:35 +0100

    .

--- Vecka 5/Genomgang/Pages/Index.cshtml.cs
+++ Vecka 5/Genomgang/Pages/Index.cshtml.cs
@@ -5,9 +5,11 @@ namespace Genomgang.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; } = "";
+
         public void OnGet()
         {
-
+            Message = "Hello from Code Behind!";
         }
     }
 }
