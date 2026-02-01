061a9d56b7ee0031b2447e65ac9418937c78e47e
hassanNewton <hassan.hussin@newton.se>
2026-02-01 10:27:16 +0100

    .

--- Vecka 5/SignUp/Models/UserModel.cs
+++ Vecka 5/SignUp/Models/UserModel.cs
@@ -1,12 +1,16 @@
-﻿namespace SignUp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SignUp.Models
 {
     public class UserModel
     {
-
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Email { get; set; }
     }
 }
